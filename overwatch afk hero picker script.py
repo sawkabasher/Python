7ed9552369f9b8383b85433ecd8f6cb0be1f49a2
Aleksandr Dzikajev <aleksandr.dzikajev@gmail.com>
2019-08-09 15:14:36 +0300

    Add files via upload

--- overwatch afk hero picker script.py
+++ overwatch afk hero picker script.py
@@ -1,3 +1,4 @@
+import json
 import time 
 import threading
 import keyboard
@@ -8,39 +9,54 @@ from gtts import gTTS
 import playsound
 import os.path
 
-profiles = profiles = (('widowmaker',1410,910),
-                       ('sombra',1177,910),
-                       ('tracer',1350,910),
-                        ('mercy',1716,910),
-                        ('ana',1486,910),
-                       ('mccree',884,910),
-                        ('genji',712,910),
-                       ('wrecking ball',402,910),
-                       ('zArya',460,910))
-profile = profiles[0]
+with open('heroes.json', 'r') as f:
+    heroes_dict = json.load(f)
+
+for hero in heroes_dict:
+    print(hero['Name'], hero['x_pos'], hero['y_pos'])
+
+print(len(heroes_dict))
+print(heroes_dict[1]['Name'])
+
+
+profile = heroes_dict[0]
+print(profile['x_pos'])
+
 
 start_stop_key = Key.f2
+show_mouse_position_key = Key.f3
 switch_profile_key = Key.f4
 exit_key = Key.f12
 
 if os.path.exists('sounds'):
+    for hero in heroes_dict:
+        if os.path.isfile('sounds\{}.mp3'.format(hero['Name'])):
+            pass
+        else:
+            
+            tts = gTTS(text=hero['Name'], lang="en")
+            tts.save('sounds\{}.mp3'.format(hero['Name']))
+            print('{}.mp3 created !!!'.format(hero['Name']))
     pass
 else:
     os.mkdir('sounds')
     tts = gTTS(text="press F4 to switch between profiles and press F2 to start", lang="en")
     tts.save('sounds\welcome.mp3')
+    print('welcome.mp3 created !!!')
+    
     tts = gTTS(text="start", lang="en")
     tts.save('sounds\start.mp3')
+    print('start.mp3 created !!!')
+    
     tts = gTTS(text="stop", lang="en")
     tts.save('sounds\stop.mp3')
-
-    for row in profiles:
-        tts = gTTS(text=row[0], lang="en")
-        tts.save('sounds\{}.mp3'.format(row[0]))
-
-
-
-
+    print('stop.mp3 created !!!')
+    
+    for hero in heroes_dict:
+        tts = gTTS(text=hero['Name'], lang="en")
+        tts.save('sounds\{}.mp3'.format(hero['Name']))
+        print('{}.mp3 created !!!'.format(hero['Name']))
+        
 mouse = Controller()
 
 class Main(threading.Thread):
@@ -51,7 +67,7 @@ class Main(threading.Thread):
 
         playsound.playsound('sounds\welcome.mp3', False)
         time.sleep(4)
-        playsound.playsound('sounds\{}.mp3'.format(profile[0]),False)
+        playsound.playsound('sounds\{}.mp3'.format(profile['Name']),False)
         
         super(Main, self).__init__()
         self.running = False
@@ -72,7 +88,7 @@ class Main(threading.Thread):
         while self.program_running:
             while self.running:
                 print(profile)
-                mouse.position = (profile[1],profile[2])
+                mouse.position = (profile['x_pos'],profile['y_pos'])
                 mouse.click(Button.left,2)
                 time.sleep(0.3)
 
@@ -92,20 +108,22 @@ def on_press(key):
             main_thread.start_clicking()
             print('if main_thread.running == False')
             playsound.playsound('sounds\start.mp3', False)
-        
-    
-    elif key == switch_profile_key:
-        
-        if profiles.index(profile) < (len(profiles) - 1):
-            profile = profiles[profiles.index(profile) + 1]
-        else:
-            profile = profiles[0]
-        playsound.playsound('sounds\{}.mp3'.format(profile[0]), False)
-        print('{}/{}'.format(profiles.index(profile), len(profiles) - 1))
-        print('x:{}, y:{}'.format(profile[1], profile[2]))
-
     elif key == exit_key:
         main_thread.exit5listener.stop()
+    elif key == show_mouse_position_key:
+        print(mouse.position)
+   
+    elif key == switch_profile_key:
+        
+        if heroes_dict.index(profile) < (len(heroes_dict) - 1):
+            profile = heroes_dict[heroes_dict.index(profile) + 1]
+        else:
+            profile = heroes_dict[0]
+        playsound.playsound('sounds\{}.mp3'.format(profile["Name"]), False)
+        print('{}/{}'.format(heroes_dict.index(profile), len(heroes_dict) - 1))
+        print('x:{}, y:{}'.format(profile['x_pos'], profile['y_pos']))
+
+print(heroes_dict.index(profile))
         
 with Listener(on_press = on_press) as listener:
     listener.join()
